e05c53d37b2251fc030a92d2d66bb6e96b1c3452
arkn37.dev <arkn37.dev@outlook.com>
2025-07-30 03:17:15 -0600

    build: add cors package

--- back/index.js
+++ back/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db/config");
 
 const app = express();
 const PORT = 8080;
+
+const config = {
+  origin: "*",
+  methods: "*",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: true,
+};
+
+app.use(cors(config));
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use(require("./routes"));
